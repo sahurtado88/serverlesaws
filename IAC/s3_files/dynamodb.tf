1256d0b1f640368f89237d7f622defa76e70fa04
Sergio Hurtado <sahurtad@gmail.com>
2025-01-15 17:39:47 -0500

    add dynamo tables

---
+++ IAC/s3_files/dynamodb.tf
@@ -0,0 +1,68 @@
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "Chat-Messages"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "ConversationId"
+  range_key      = "Timestamp"
+
+  attribute {
+    name = "ConversationId"
+    type = "S"
+  }
+
+  attribute {
+    name = "Timestamp"
+    type = "N"
+  }
+
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = true
+  }
+
+  tags = {
+    Name        = "dynamodb-table-1"
+    Environment = "production"
+  }
+}
+
+resource "aws_dynamodb_table" "table2" {
+  name           = "Chat-Conversations"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "ConversationId"
+  range_key      = "Username"
+
+  attribute {
+    name = "ConversationId"
+    type = "S"
+  }
+
+  attribute {
+    name = "Username"
+    type = "S"
+  }
+
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = true
+  }
+
+  global_secondary_index {
+    name               = "Username-ConversationId-index"
+    hash_key           = "Username"
+    range_key          = "ConversationId"
+    write_capacity     = 1
+    read_capacity      = 1
+    projection_type    = "ALL"
+  }
+
+  tags = {
+    Name        = "dynamodb-table-2"
+    Environment = "production"
+  }
+}
--- IAC/s3_files/main.tf
+++ IAC/s3_files/main.tf
@@ -107,15 +107,6 @@ resource "aws_iam_policy" "iam_policy_for_lambda" {
 EOF
 }
 
-resource "aws_lambda_permission" "apigw_lambda" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda_func.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_api_gateway_rest_api.chat_api.execution_arn}/*/*/*"
-}
-
 resource "aws_iam_role_policy_attachment" "lambda_basic" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   role = aws_iam_role.lambda_role.name
@@ -142,80 +133,3 @@ resource "aws_lambda_function" "lambda_func" {
   runtime       = "nodejs22.x"
 }
 
-## api gateway
-
-resource "aws_api_gateway_rest_api" "chat_api" {
-  name        = "Chat-api"
-  description = "My API Gateway to chat"
-
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-}
-
-resource "aws_api_gateway_resource" "root" {
-  rest_api_id = aws_api_gateway_rest_api.chat_api.id
-  parent_id   = aws_api_gateway_rest_api.chat_api.root_resource_id
-  path_part   = "{proxy+}"
-}
-
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.chat_api.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = "ANY"
-  authorization = "NONE"
-}
-resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.chat_api.id
-  resource_id             = aws_api_gateway_resource.root.id
-  http_method             = aws_api_gateway_method.proxy.http_method
-  integration_http_method = "POST"
-  type                    = "AWS"
-  uri                     = aws_lambda_function.lambda_func.invoke_arn
-}
-
-resource "aws_api_gateway_method_response" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.chat_api.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = aws_api_gateway_method.proxy.http_method
-  status_code = "200"
-    //cors section
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true,
-    "method.response.header.Access-Control-Allow-Methods" = true,
-    "method.response.header.Access-Control-Allow-Origin" = true
-  }
-}
-
-resource "aws_api_gateway_integration_response" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.chat_api.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = aws_api_gateway_method.proxy.http_method
-  status_code = aws_api_gateway_method_response.proxy.status_code
-
-  //cors
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" =  "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
-}
-
-  depends_on = [
-    aws_api_gateway_method.proxy,
-    aws_api_gateway_integration.lambda_integration
-  ]
-}
-
-resource "aws_api_gateway_deployment" "deployment" {
-  depends_on = [
-    aws_api_gateway_integration.lambda_integration,
-  ]
-
-  rest_api_id = aws_api_gateway_rest_api.chat_api.id
-}
-
-resource "aws_api_gateway_stage" "example" {
-  deployment_id = aws_api_gateway_deployment.deployment.id
-  rest_api_id   = aws_api_gateway_rest_api.chat_api.id
-  stage_name    = "prod"
-}
